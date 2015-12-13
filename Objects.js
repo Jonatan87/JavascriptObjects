c1bc77478430f62a0f1d000a2923610b6105f6c2
Jonatan <jonatan.g.petersson@gmail.com>
2015-12-13 09:29:51 +0100

    .

--- Objects.js
+++ Objects.js
@@ -1,3 +1,20 @@
 /**
  * Created by Jonatan on 2015-12-13.
  */
+
+var myObject = {item1: "1", item2: "2"};
+
+function Screen()
+{
+    this.Width = 2;
+    this.Heigth = 10;
+}
+
+Screen.prototype.Color = "Black";
+
+var screen = new Screen();
+
+alert(screen.Color);
+
+alert(myObject.item1);
+
